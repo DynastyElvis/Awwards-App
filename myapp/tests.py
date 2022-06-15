0f58937d385022dea98b43c646ecd8a032727274
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-15 13:19:48 +0300

    bg:fixedurlspatterns and tests bugs

--- myapp/tests.py
+++ myapp/tests.py
@@ -23,3 +23,31 @@ class RatingTest(TestCase):
         self.rating.save()
         rating = Rating.get_ratings(post_id=id)
         self.assertTrue(len(rating) == 1)
+        class PostTest(TestCase):
+        def setUp(self):
+        self.user = User.objects.create(id=1, username='caleb')
+        self.post = Post.objects.create(id=1, title='test', photo='https://ucarecdn.com/0ccf61ff-508e-46c6-b713-db51daa6626e', description='desc',
+                                        user=self.user, link='http://ur.coml')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post))
+
+    def test_save_post(self):
+        self.post.save_post()
+        post = Post.objects.all()
+        self.assertTrue(len(post) > 0)
+
+    def test_get_posts(self):
+        self.post.save()
+        posts = Post.all_posts()
+        self.assertTrue(len(posts) > 0)
+
+    def test_search_post(self):
+        self.post.save()
+        post = Post.search_projects('test')
+        self.assertTrue(len(post) > 0)
+
+    def test_delete_post(self):
+        self.post.delete_post()
+        post = Post.search_projects('test')
+        self.assertTrue(len(post) < 1)
--- myapp/urls.py
+++ myapp/urls.py
@@ -3,3 +3,28 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.authtoken.views import obtain_auth_token
+
+
+
+urlpatterns=[
+    re_path('^$',views.welcome,name='welcome'),
+    re_path('^$',views.index,name='index'),
+    re_path('login/',views.signin,name='login'),
+    re_path('register/',views.register,name='register'),
+    re_path('signout/',views.signout,name='signout'),
+    re_path('profile/',views.profile,name='profile'),
+    re_path('addpost/',views.addpost,name='addpost'),
+    re_path('new-project/', views.postproject, name='newproject'),
+    re_path('update', views.update_profile, name='update'),
+    re_path('search/', views.search_project, name='search'),
+    re_path(r'^vote/(?P<post_id>\d+)?$', views.project, name='vote'), 
+    re_path(r'^api/post/$', views.PostItems.as_view()),
+    re_path(r'^api/profile/$', views.ProfileItems.as_view()),
+    re_path(r'^api-token-auth/', obtain_auth_token),
+    re_path('api/profile',views.ProfileItems.as_view(), name='apiprofiles'),
+
+
+
+   
+    
+]

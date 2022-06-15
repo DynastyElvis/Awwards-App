960668f9bd5f834cdfd96b178206d6f4ac5900e3
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-15 13:22:32 +0300

    bg:fixed ratings and posts bugs on models

--- myapp/models.py
+++ myapp/models.py
@@ -4,6 +4,8 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from cloudinary.models import CloudinaryField
 
+
+
 # Create your models here.
 class Profile(models.Model):
     user= models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
@@ -51,3 +53,38 @@ class Post(models.Model):
     @classmethod
     def search_projects(cls,title):
         return cls.objects.filter(title__icontains=title).all()    
+
+class Rating(models.Model):
+    rating = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
+    )
+
+    design = models.IntegerField(choices=rating, default=0, blank=True)
+    usability = models.IntegerField(choices=rating, blank=True)
+    content = models.IntegerField(choices=rating, blank=True)
+    score = models.FloatField(default=0, blank=True)
+    design_average = models.FloatField(default=0, blank=True)
+    usability_average = models.FloatField(default=0, blank=True)
+    content_average = models.FloatField(default=0, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='rater')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='ratings', null=True)
+
+    def save_rating(self):
+        self.save()
+
+    @classmethod
+    def get_ratings(cls, id):
+        ratings = Rating.objects.filter(post_id=id).all()
+        return ratings
+
+    def __str__(self):
+        return f'{self.post} Rating'

b301800b80fcb2a36aa8354a87cbc7a037b58fba
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-15 13:18:58 +0300

    bg:fixedurlss bugs

---
+++ myapp/urls.py
@@ -0,0 +1,5 @@
+from django.urls import re_path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework.authtoken.views import obtain_auth_token

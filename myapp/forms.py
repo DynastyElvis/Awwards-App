8cc2664b6cf85841ec144bbd4622b2f2b31caeae
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-15 13:14:46 +0300

    ft: forms logic

---
+++ myapp/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Post, Profile,Rating
+from django.contrib.auth.models import User
+
+
+
+class PostForm(forms.ModelForm):
+    photo=forms.ImageField(label="")
+    class Meta:
+        model=Post
+        fields=('photo','title','link','description')
+
+class UpdateUserForm(forms.ModelForm):
+    email=forms.EmailField(max_length=254,help_text='Required.Inform a valid email addres')
+    class Meta:
+        model= User
+        fields=('username','email')
+
+class UpdateUserProfileForm(forms.ModelForm):
+    email=forms.EmailField(max_length=254,help_text='Required.Inform a valid email addres')
+    class Meta:
+        model= Profile
+        fields=('name','bio','profile_picture','location','contact_email')
+
+
